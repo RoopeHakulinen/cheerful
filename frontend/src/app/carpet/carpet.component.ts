bdcf87d25f2080c3e220e7ce1cf15f85f2537e6b
Roope Hakulinen <roope.hakulinen@gmail.com>
2017-12-28 22:58:06 +0200

    Improve mobile-friendliness.

--- frontend/src/app/carpet/carpet.component.ts
+++ frontend/src/app/carpet/carpet.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { DragulaService } from 'ng2-dragula';
 import { Carpet } from '../carpet';
 import { ChoreographyFrame } from '../choreography-frame';
@@ -25,7 +26,7 @@ export class CarpetComponent implements OnInit, OnChanges {
   segments: any[];
   tileDimension = 50;
 
-  constructor(private dragulaService: DragulaService) {
+  constructor(private dragulaService: DragulaService, private sanitizer: DomSanitizer) {
     this.dragulaService.drop.subscribe((value) => {
       const first = parseInt(value[2].getAttribute('index'), 10);
       const second = parseInt(value[3].getAttribute('index'), 10);
@@ -39,6 +40,18 @@ export class CarpetComponent implements OnInit, OnChanges {
   ngOnChanges(changes: SimpleChanges) {
     if (changes.carpet && changes.carpet.previousValue !== changes.carpet.currentValue) {
       this.segments = Array(changes.carpet.currentValue.segments).fill(0);
+      this.setTileDimension();
     }
+  }
+
+  @HostListener('window:resize')
+  setTileDimension() {
+    const el = document.querySelector('.carpet').parentElement.parentElement;
+    this.tileDimension = Math.min((el.clientWidth - 20) / this.carpet.width, 50);
+    console.log(this.tileDimension);
+  }
+
+  sanitize(val: string) {
+    return this.sanitizer.bypassSecurityTrustStyle(val);
   }
 }
