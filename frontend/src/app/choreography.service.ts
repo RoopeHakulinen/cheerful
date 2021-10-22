5cc67196bb6e5dab5bfd2d42f2313136defc2ef4
VMakinen <viltsu.m@hotmail.com>
2021-10-22 16:00:00 +0300

    Add a few people more to the choreography to help test the app.

--- frontend/src/app/choreography.service.ts
+++ frontend/src/app/choreography.service.ts
@@ -34,16 +34,22 @@ export class ChoreographyService {
         verticalSegments: 6,
       },
       people: [
-        { personId: 1, color: '#ff0000' },
+        { personId: 1, color: '#2F8F9F' },
         { personId: 2, color: '#2F8F9F' },
-        { personId: 3, color: '#0000ff' },
-        { personId: 4, color: '#880000' },
-        { personId: 5, color: '#220000' },
-        { personId: 6, color: '#ff0000' },
+        { personId: 3, color: '#2F8F9F' },
+        { personId: 4, color: '#2F8F9F' },
+        { personId: 5, color: '#2F8F9F' },
+        { personId: 6, color: '#2F8F9F' },
         { personId: 7, color: '#2F8F9F' },
-        { personId: 8, color: '#0000ff' },
-        { personId: 9, color: '#880000' },
-        { personId: 10, color: '#220000' },
+        { personId: 8, color: '#2F8F9F' },
+        { personId: 9, color: '#2F8F9F' },
+        { personId: 10, color: '#2F8F9F' },
+        { personId: 11, color: '#2F8F9F' },
+        { personId: 12, color: '#2F8F9F' },
+        { personId: 13, color: '#2F8F9F' },
+        { personId: 14, color: '#2F8F9F' },
+
+
       ],
     },
     {
@@ -73,7 +79,6 @@ export class ChoreographyService {
   }
 
   getChoreographiesById(id: number): Observable<Choreography> {
-    return timer(500).pipe(mapTo(this.choreographies.find(choreography => choreography.id === id)!));
+    return timer(0).pipe(mapTo(this.choreographies.find(choreography => choreography.id === id)!));
   }
-
 }
