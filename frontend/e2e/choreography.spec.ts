77123f3dc89c88565bdb62a240dead3d81375a0f
VMakinen <viltsu.m@hotmail.com>
2021-10-28 14:47:07 +0300

    Remove unnecessary tests.

--- frontend/e2e/choreography.spec.ts
+++ frontend/e2e/choreography.spec.ts
@@ -1,19 +1,9 @@
 import { test } from '@playwright/test';
 
 test.describe('Frame manager', function () {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/choreographies/1');
-  });
 
 });
 
 test.describe('Carpet', function () {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/choreographies/1');
-  });
-
-  test('Move a person test', async ({ page }) => {
-
-  });
 
 });
