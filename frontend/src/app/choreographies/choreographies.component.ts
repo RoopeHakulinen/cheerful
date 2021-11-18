2d2fa9495e4793dcf552546da3c8663c5ec85c81
VMakinen <viltsu.m@hotmail.com>
2021-11-18 10:32:30 +0200

    Add async pipes to carpet names and add the choreographies as a list to the choreographies page using ngSwitchCase.

--- frontend/src/app/choreographies/choreographies.component.ts
+++ frontend/src/app/choreographies/choreographies.component.ts
@@ -2,18 +2,17 @@ import { Component } from '@angular/core';
 import { ChoreographyService } from '../choreography.service';
 import { Choreography } from '../choreography';
 import { Observable } from 'rxjs';
+import { QueryOutput } from 'rx-query';
 
 @Component({
   selector: 'app-choreographies',
   templateUrl: './choreographies.component.html',
-  styleUrls: ['./choreographies.component.scss']
+  styleUrls: ['./choreographies.component.scss'],
 })
 export class ChoreographiesComponent {
-
-  choreographies$: Observable<Choreography[]>;
+  choreographies$: Observable<QueryOutput<Choreography[]>>;
 
   constructor(public choreographyService: ChoreographyService) {
     this.choreographies$ = this.choreographyService.getChoreographies();
   }
-
 }
