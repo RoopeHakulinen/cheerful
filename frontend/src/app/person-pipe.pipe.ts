09bff6a502322e3894045b188b06f8eeaff4e099
VMakinen <viltsu.m@hotmail.com>
2021-11-18 12:48:50 +0200

    Change the person-pipe to use the getPersonById method of the choreography.service.ts. Also add the translation texts for loading and error.

--- frontend/src/app/person-pipe.pipe.ts
+++ frontend/src/app/person-pipe.pipe.ts
@@ -1,18 +1,21 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { PeopleService } from './people.service';
 import { Person } from './people';
+import { filter, map, Observable } from 'rxjs';
 
 @Pipe({
   name: 'person',
 })
 export class PersonPipe implements PipeTransform {
-  constructor(private peopleService: PeopleService) {
-  }
+  constructor(private peopleService: PeopleService) {}
 
-  transform(id: number | null): Person {
+  transform(id: number | null): Observable<Person> {
     if (id === null) {
       throw new Error(`No such person id ${id} when looking up a person.`);
     }
-    return this.peopleService.getPersonById(id);
+    return this.peopleService.getPersonById(id).pipe(
+      filter((queryOutput) => queryOutput.status === 'success'),
+      map((queryOutput) => queryOutput.data!)
+    );
   }
 }
