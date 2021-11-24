6191b84399b01c147d62ab52533a68f78f67bf75
VMakinen <viltsu.m@hotmail.com>
2021-11-24 16:27:01 +0200

    Revert the removal of people$ -property.

--- frontend/src/app/people-manager/people-manager.component.ts
+++ frontend/src/app/people-manager/people-manager.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { PeopleService } from '../people.service';
 import { Person } from '../people';
-import { filter, map } from 'rxjs';
+import { filter, map, Observable } from 'rxjs';
+import { QueryOutput } from 'rx-query';
 import { Choreography } from '../choreography';
 
 export interface PersonWithChoreographies extends Person {
@@ -16,11 +17,13 @@ export interface PersonWithChoreographies extends Person {
 export class PeopleManagerComponent {
   displayedColumns: string[] = ['id', 'name', 'choreographies'];
   columnsToDisplay: string[] = this.displayedColumns.slice();
+
+  people$: Observable<QueryOutput<PersonWithChoreographies[]>>;
   data$!: any;
 
   constructor(private peopleService: PeopleService) {
-    this.peopleService
-      .getPeople()
+    this.people$ = this.peopleService.getPeople();
+    this.people$
       .pipe(
         filter((queryOutput) => queryOutput.status === 'success'),
         map((queryOutput) => queryOutput.data!)
