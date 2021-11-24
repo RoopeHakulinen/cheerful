7517878a00a61076d90e20adc6ea5cc044a5ebd1
VMakinen <viltsu.m@hotmail.com>
2021-11-24 16:25:30 +0200

    Remove unused people$ -property.

--- frontend/src/app/people-manager/people-manager.component.ts
+++ frontend/src/app/people-manager/people-manager.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { PeopleService } from '../people.service';
 import { Person } from '../people';
-import { filter, map, Observable } from 'rxjs';
-import { QueryOutput } from 'rx-query';
+import { filter, map } from 'rxjs';
 import { Choreography } from '../choreography';
 
 export interface PersonWithChoreographies extends Person {
@@ -17,13 +16,11 @@ export interface PersonWithChoreographies extends Person {
 export class PeopleManagerComponent {
   displayedColumns: string[] = ['id', 'name', 'choreographies'];
   columnsToDisplay: string[] = this.displayedColumns.slice();
-
-  people$: Observable<QueryOutput<PersonWithChoreographies[]>>;
   data$!: any;
 
   constructor(private peopleService: PeopleService) {
-    this.people$ = this.peopleService.getPeople();
-    this.people$
+    this.peopleService
+      .getPeople()
       .pipe(
         filter((queryOutput) => queryOutput.status === 'success'),
         map((queryOutput) => queryOutput.data!)
