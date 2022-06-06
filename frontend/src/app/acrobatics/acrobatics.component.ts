d301a8f650fe6d869e13afe926137d9cbe029ec0
rovai1 <roni.vainio@edu.tampere.fi>
2022-06-06 13:59:05 +0300

    Add acrobatics view.

--- frontend/src/app/acrobatics/acrobatics.component.ts
+++ frontend/src/app/acrobatics/acrobatics.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
 
+export interface Exercise {
+  id: number;
+  name: string;
+  description: string;
+}
+
 export interface Acrobatic {
   id: number;
   name: string;
+  description: string;
   icon: string;
   difficulty: number;
+  exercises: Exercise[];
 }
+
+export const acrobatics: Acrobatic[] = [
+  {
+    id: 1, name: 'JUMP', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent porttitor eget felis vitae dictum.', icon: 'settings_accessibility', difficulty: 2, exercises: [
+      { id: 1, name: 'Ponnista', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent porttitor eget felis vitae dictum.' },
+      { id: 2, name: 'Laskeudu', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent porttitor eget felis vitae dictum.' }
+    ]
+  },
+  { id: 2, name: 'STAND', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent porttitor eget felis vitae dictum.', icon: 'accessibility', difficulty: 1, exercises: [] },
+  { id: 3, name: 'WALK', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent porttitor eget felis vitae dictum.', icon: 'directions_walk', difficulty: 1, exercises: [] },
+  { id: 4, name: 'RUN', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent porttitor eget felis vitae dictum.', icon: 'directions_run', difficulty: 2, exercises: [] },
+  { id: 5, name: 'WAVE', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent porttitor eget felis vitae dictum.', icon: 'emoji_people', difficulty: 1, exercises: [] },
+  { id: 6, name: 'THROW', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent porttitor eget felis vitae dictum.', icon: 'sports_handball', difficulty: 3, exercises: [] },
+  { id: 7, name: 'LIFT', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent porttitor eget felis vitae dictum.', icon: 'sports_kabaddi', difficulty: 4, exercises: [] },
+]
 
 @Component({
   selector: 'app-acrobatics',
@@ -13,13 +36,5 @@ export interface Acrobatic {
   styleUrls: ['./acrobatics.component.scss'],
 })
 export class AcrobaticsComponent {
-  availableAcrobatics: Acrobatic[] = [
-    { id: 1, name: 'Jump', icon: 'settings_accessibility', difficulty: 2 },
-    { id: 2, name: 'Stand', icon: 'accessibility', difficulty: 1 },
-    { id: 3, name: 'Walk', icon: 'directions_walk', difficulty: 1 },
-    { id: 4, name: 'Run', icon: 'directions_run', difficulty: 2 },
-    { id: 5, name: 'Wave', icon: 'emoji_people', difficulty: 1 },
-    { id: 6, name: 'Throw', icon: 'sports_handball', difficulty: 3 },
-    { id: 7, name: 'Lift', icon: 'sports_kabaddi', difficulty: 4 },
-  ];
+  availableAcrobatics = acrobatics;
 }
