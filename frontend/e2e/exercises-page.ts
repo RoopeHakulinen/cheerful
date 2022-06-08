d7393a5da4837746f17a39a4330ee7efdb5f4c49
rovai1 <roni.vainio@edu.tampere.fi>
2022-06-08 13:58:17 +0300

    Add filtering for exercises and update to exercises playwright test.

--- frontend/e2e/exercises-page.ts
+++ frontend/e2e/exercises-page.ts
@@ -22,4 +22,25 @@ export class ExercisesPage {
     async checkExercisePageIsVisible(exercise: string): Promise<void> {
         await expect(this.page.locator(`mat-toolbar .title:has-text("${exercise}")`).first()).toBeVisible();
     }
+
+    async openFilterBar(): Promise<void> {
+        await this.page.click('mat-expansion-panel-header .mat-expansion-indicator');
+    }
+
+    async updateQuery(query: string): Promise<void> {
+        await this.page.fill('.example-form-field .query-input', query);
+    }
+
+    async checkExerciseIsVisible(query: string): Promise<void> {
+        await expect(this.page.locator(`app-exercises-listing .single-exercise:has-text("${query}")`).first()).toBeVisible();
+    }
+
+    async updateDifficultyRangeCheck(minDifficulty: number, maxDifficulty: number): Promise<void> {
+        await this.page.fill('.minDifficulty-input', minDifficulty.toString());
+        await this.page.fill('.maxDifficulty-input', maxDifficulty.toString());
+    }
+
+    async checkExerciseIsNotVisible(query: string): Promise<void> {
+        await expect(this.page.locator(`app-exercises-listing .single-exercise:has-text("${query}")`).first()).not.toBeVisible();
+    }
 }
