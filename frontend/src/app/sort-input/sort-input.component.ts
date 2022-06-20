2ab3761c17ca3c763a5b2ad0675f7bfda2aa8962
rovai1 <roni.vainio@edu.tampere.fi>
2022-06-20 15:53:37 +0300

    Add exercise sorting.

---
+++ frontend/src/app/sort-input/sort-input.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+export interface SortOption {
+  name: string;
+  value: string;
+}
+
+@Component({
+  selector: 'app-sort-input',
+  templateUrl: './sort-input.component.html',
+  styleUrls: ['./sort-input.component.scss']
+})
+export class SortInputComponent {
+
+  @Input()
+  options: SortOption[] = [];
+
+  sortMethod: string = 'name';
+
+  @Output()
+  sortWith = new EventEmitter<string>();
+
+  selectMethod(method: string): void {
+    this.sortWith.emit(method);
+  }
+}
