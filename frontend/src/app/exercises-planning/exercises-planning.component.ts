1e4c3800065d74d900469e267771c5eb2beb49f3
rovai1 <roni.vainio@edu.tampere.fi>
2022-06-28 14:33:07 +0300

    Add exercise planning.

---
+++ frontend/src/app/exercises-planning/exercises-planning.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exercises-planning',
+  templateUrl: './exercises-planning.component.html',
+  styleUrls: ['./exercises-planning.component.scss']
+})
+export class ExercisesPlanningComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
