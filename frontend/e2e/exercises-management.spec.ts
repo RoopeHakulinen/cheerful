7a8b94c565f83e5c88cf2f7d7cd9c8dc766c4706
rovai1 <roni.vainio@edu.tampere.fi>
2022-06-28 11:47:19 +0300

    Try to fix test.

--- frontend/e2e/exercises-management.spec.ts
+++ frontend/e2e/exercises-management.spec.ts
@@ -2,11 +2,11 @@ import { expect, test } from '@playwright/test';
 import { ExercisesPage } from './exercises-page';
 
 test.describe('Exercises management', function () {
-  test('Add an exercise', async ({ page }) => {
+  test.only('Add an exercise', async ({ page }) => {
     const exercisesPage = new ExercisesPage(page);
 
     const name = 'Test exercise';
-    const description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac nisi ut mauris maximus maximus. Phasellus pretium augue est, vitae tempor quam tempus sit amet.';
+    const description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac nisi ut mauris maximus maximus. Phasellus pretium augue est, vitae tempor quam tempus sit amet...';
     const tag = 'Akrobatiaa';
     const difficulty = 3;
 
