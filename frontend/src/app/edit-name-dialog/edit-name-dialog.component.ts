03205d0621827c0865a4aa3a9d8f4b9f5400b100
rovai1 <roni.vainio@edu.tampere.fi>
2022-07-19 12:08:45 +0300

    Remove unused code.

--- frontend/src/app/edit-name-dialog/edit-name-dialog.component.ts
+++ frontend/src/app/edit-name-dialog/edit-name-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 export interface EditNameDialogData {
@@ -15,8 +15,4 @@ export class EditNameDialogComponent{
     public dialogRef: MatDialogRef<EditNameDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: EditNameDialogData,
   ) {}
-
-  onNoClick(): void {
-    this.dialogRef.close();
-  }
 }
