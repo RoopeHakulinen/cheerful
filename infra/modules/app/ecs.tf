62cd95969129300204ed3b4a9c0cc29cf346415e
Roope Hakulinen <roope.hakulinen@gmail.com>
2022-11-02 10:17:39 +0200

    Downgrade the specs for the ECS task.

--- infra/modules/app/ecs.tf
+++ infra/modules/app/ecs.tf
@@ -22,15 +22,15 @@ resource "aws_ecs_task_definition" "cheerful" {
   requires_compatibilities = ["FARGATE"]
   task_role_arn            = aws_iam_role.task.arn
   execution_role_arn       = aws_iam_role.execution.arn
-  cpu                      = 1024
-  memory                   = 2048
+  cpu                      = 256
+  memory                   = 512
 
   container_definitions = <<DEFINITION
 [
   {
     "image": "${data.aws_ecr_repository.app.repository_url}:latest",
-    "cpu": 1024,
-    "memory": 2048,
+    "cpu": 256,
+    "memory": 512,
     "name": "cheerful-app-${var.environment}",
     "networkMode": "awsvpc",
     "portMappings": [
