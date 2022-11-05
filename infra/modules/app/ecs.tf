2d13b816f92623434b9b43e65f688da98b076141
VMakinen <viltsu.m@hotmail.com>
2022-11-05 17:40:03 +0200

    Revert memory and CPU changes.

--- infra/modules/app/ecs.tf
+++ infra/modules/app/ecs.tf
@@ -22,8 +22,8 @@ resource "aws_ecs_task_definition" "cheerful" {
   requires_compatibilities = ["FARGATE"]
   task_role_arn            = aws_iam_role.task.arn
   execution_role_arn       = aws_iam_role.execution.arn
-  cpu                      = 256
-  memory                   = 512
+  cpu                      = 1024
+  memory                   = 2048
 
   container_definitions = <<DEFINITION
 [
